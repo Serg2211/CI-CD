cc323c586ad89bed9d2a8a95b121e568e84c43b7
Serg2211 <goryashins@yandex.ru>
2024-03-09 15:15:17 +0000

    commit 30

--- k8s.tf
+++ k8s.tf
@@ -49,8 +49,8 @@ resource "yandex_compute_instance" "worker-nodes" {
   allow_stopping_for_update = true
 
   resources {
-    cores  = 4
-    memory = 4
+    cores  = 2
+    memory = 2
     core_fraction = 100
   }
 
