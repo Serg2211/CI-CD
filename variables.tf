be9483eac17c1e7df9f70b92bef7426ba8fa0945
Serg2211 <goryashins@yandex.ru>
2024-03-09 07:23:05 +0000

    commit 9

--- variables.tf
+++ variables.tf
@@ -26,6 +26,6 @@ variable "default_zone" {
 variable "cidr" {
   type    = map(list(string))
   default = {
-    blocks = ["192.168.2.0/24", "192.168.3.0/24"]....
+    blocks = ["192.168.2.0/24", "192.168.3.0/24"]
   }
 }
