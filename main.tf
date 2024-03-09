a87ed90473b27272bdb18266cd84ed584e565651
Serg2211 <goryashins@yandex.ru>
2024-03-09 13:31:21 +0000

    commit 23

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     endpoint       = "storage.yandexcloud.net"
     bucket         = "diplom-bucket"
-    key            = "terraform/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ru-central1"
     skip_region_validation      = true
     skip_credentials_validation = true
