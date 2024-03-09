db2844ffdb8e7102b1a66b9bf8585602ec86e6b2
Serg2211 <goryashins@yandex.ru>
2024-03-09 07:21:53 +0000

    commit 8

--- variables.tf
+++ variables.tf
@@ -1,7 +1,31 @@
+###cloud vars
+variable "token" {
+  type        = string
+  default     = "y0_AgAAAAAQK2kLAATuwQAAAAD7Uif9AACBsVW2N6xBsIj9nUxGI45XIUd5lA"
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1g8986h7kafrin500ch"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1gjsnlha3fii86tav22"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
 
 variable "cidr" {
   type    = map(list(string))
   default = {
-    blocks = ["192.168.2.0/24", "192.168.3.0/24"]    
+    blocks = ["192.168.2.0/24", "192.168.3.0/24"]....
   }
 }
